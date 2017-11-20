d0edfab554ded167ec2cf624268fc1f6b20d0da2
OITWPsupport <oitcommunications@boisestate.edu>
2017-11-20 09:52:39 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,14 +4,15 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
+$option1 = 'message_I';
+delete_option($option1);
+
 $options = array("message_I", "message_II", "message_III", "message_IV", "low_score", "mid_score", "high_score"); 
 
 for ($i = 0; $i < $options.count(); $i++) {
 	delete_option($options[$i]);
 	delete_site_option($options[$i]);
 }
-
-delete_option("message_I");
  
 // drop a custom database table
 //global $wpdb;
